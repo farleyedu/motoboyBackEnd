a78a3cd621bd00bd042f752e6fb494493077d759
Farley Eduardo <farleysilvae@gmail.com>
2025-07-30 20:26:46 -0300

    confirmador de codigo do ifood inserido

---
+++ DTOs/EntregaIfood.cs
@@ -0,0 +1,14 @@
+﻿public class ConfirmarEntregaRequest
+{
+    public string Localizador { get; set; }
+    public string CodigoCliente { get; set; }
+}
+
+public class IfoodLocalizadorResponse
+{
+    public string orderId { get; set; }
+    public string customerName { get; set; }
+    public string shortId { get; set; }
+    public string deliveryMethod { get; set; }
+    public string storeType { get; set; }
+}
